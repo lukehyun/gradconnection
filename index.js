7a78f49a704bc7ee7a4737574769c1bf0834201f
unknown <byunghun.hyun26@gmail.com>
2022-06-17 10:06:36 +0900

    get title,url,cloingdate

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+let titles;
+let urls;
+let closingdates;
 
+async function getdata() {
+  try {
+    const reslist = await axios.get('https://au.gradconnection.com/graduate-jobs/engineering-software/')
+    let $ = cheerio.load(reslist.data);
+    for(title of $('.box-header-title'))
+	{
+		titles.push($(title).text());
+		urls.push($(title).attr('href'));
+		const resdata = await axios.get('https://au.gradconnection.com'+$(title).attr('href'));
+		$=cheerio.load(resdata.data);
+		for (data of $('.box-content-catagories.catagories-list'))
+		{
+			if($(data).first().text().indexOf('Closing Date')!=-1) closingdates.push($(data).children().remove().end().text());
+		}
+		
+	}
+  } catch(err) {
+    console.log(err);
+  }
+}
 
-axios
-  .get('https://au.gradconnection.com/graduate-jobs/engineering-software/')
-  .then(res => {
-    $ = cheerio.load(res.data);
-	$('.box-header-title').each(function(i,title){
-		console.log($(title).text());
-		console.log($(title).attr('href'));
-	});
-  })
-  .catch(error => {
-    console.error(error);
-  });
+(async function()
+{
+	
+	titles=[];
+	urls=[];
+	closingdates=[];
+
+	await getdata();
+	console.log(titles)
+	console.log(urls)
+	console.log(closingdates)
+})();
+
